2cadb574220bc3409432ec9d748b1c8672036685
Hamish Starling <hamish.starling@softwire.com>
2023-07-04 10:56:27 +0100

    Feat: Adds Fezz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,28 +1,55 @@
+function process_3(strings) {
+    return strings.concat("Fizz");
+}
+
+function process_5(strings) {
+    return strings.concat("Buzz");
+}
+
+function process_7(strings) {
+    return strings.concat("Bang");
+}
+
+function process_11(strings) {
+    return ["Bong"];
+}
+
+function process_13(strings) {
+    let i = 0;
+    strings.push("placeholder"); // make space for the new string
+    for (; i < strings.length && strings[i][0] == 'F'; ++i) {}
+    let insert_pos = i;
+    for (; i < strings.length - 1; ++i) {
+        strings[i + 1] = strings[i];
+    }
+    strings[insert_pos] = "Fezz";
+    return strings;
+}
+
 function translate_number(number) {
-    let result = "";
+    let mappings = [
+        [ 3,  process_3 ],
+        [ 5,  process_5 ],
+        [ 7,  process_7 ],
+        [ 11, process_11 ],
+        [ 13, process_13 ]
+    ]
     
-    if (number % 3 == 0) {
-        result += "Fizz";
-    }
+    let strings = [];
 
-    if (number % 5 == 0) {
-        result += "Buzz";
+    for (let [mod, handler] of mappings) {
+        if (number % mod == 0) {
+            strings = handler(strings);
+        }
     }
+   
 
-    if (number % 7 == 0) {
-        result += "Bang";
-    }
-
-    if (number % 11 == 0) {
-        result = "Bong";
-    }
-    
-    return (result == "" ? number.toString() : result);
+    return (strings.length == 0 ? number.toString() : strings.join(""));
 }
 
 // This is our main function
 function fizzbuzz() {
-    for (let i = 1; i <= 200; ++i) {
+    for (let i = 1; i <= 300; ++i) {
         console.log(translate_number(i));
     }
 }
