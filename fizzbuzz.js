c81aff46e29cb1d1b9594af1fa5408befd3d8631
Hamish Starling <hamish.starling@softwire.com>
2023-07-04 10:15:05 +0100

    Feat: Completes FizzBuzz Part 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,20 @@
+function translate_number(number) {
+    if (number % 15 == 0) {
+        return "FizzBuzz";
+    } else if (number % 3 == 0) {
+        return "Fizz";
+    } else if (number % 5 == 0) {
+        return "Buzz";
+    } else {
+        return number.toString();
+    }
+}
+
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
-
-    // Put your code here...
-
+    for (let i = 1; i <= 100; ++i) {
+        console.log(translate_number(i));
+    }
 }
 
 // Now, we run the main function:
